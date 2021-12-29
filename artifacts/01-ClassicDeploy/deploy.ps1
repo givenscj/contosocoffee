4a6962df2c0f700b39ada86e0ccf0ee8fabdaf60
Chris Givens <chris@solliance.net>
2021-12-29 20:59:20 +0000

    Initial commit

--- artifacts/01-ClassicDeploy/deploy.ps1
+++ artifacts/01-ClassicDeploy/deploy.ps1
@@ -1,17 +1,17 @@
-$sourcePath = "c:\labfiles\microsoft-mysql-developer-guide\artifacts\01-ClassicDeploy";
-$targetPath = "c:\inetpub\wwwroot\01-ClassicDeploy";
+$sourcePath = "c:\labfiles\microsoft-mysql-developer-guide\sample-php-app";
+$targetPath = "c:\inetpub\wwwroot\contosostore";
 
 # create a new IIS directory
 mkdir $targetPath;
 
 # copy the php files
-copy-item -Path "$sourcepath/src/*.*" "$targetpath"
+copy-item -Path "$sourcepath/*" "$targetpath" -Recurse
 
 #deploy the database
 $username = "root";
 $password = "";
 $server = "localhost";
-$database = "ContosoCoffee";
+$database = "contosostore";
 
 cd "C:\Program Files\MySQL\MySQL Workbench 8.0 CE"
 
@@ -19,7 +19,10 @@ cd "C:\Program Files\MySQL\MySQL Workbench 8.0 CE"
 .\mysql -h $server -u $username -e "create database $database"
 
 #setup the schema
-.\mysql -h $server -u $username $database -e "source $sourcePath/database/schema.sql"
+#.\mysql -h $server -u $username $database -e "source $sourcePath/database/schema.sql"
 
 #add the data
 #TODO
+
+#open the website
+Start-Process "http://localhost:8080"
